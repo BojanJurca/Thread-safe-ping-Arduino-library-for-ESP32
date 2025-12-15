8eb0986c709706bd333f4f245583ec47ba34e664
Bojan Jurca <42709677+BojanJurca@users.noreply.github.com>
2025-12-15 19:28:19 +0100

    Add files via upload

--- examples/Multitasking_ping/Multitasking_ping.ino
+++ examples/Multitasking_ping/Multitasking_ping.ino
@@ -1,1 +1,66 @@
-/**/
+#include <WiFi.h>
+#include <ThreadSafePing.h>
+
+
+void setup () {
+    Serial.begin (115200);
+    while (!Serial)
+        delay (10);
+
+    WiFi.begin ("YourSSID", "YourPassword"); // use your WiFi credentials
+
+    Serial.print ("Connecting ... ");
+    while (WiFi.status () != WL_CONNECTED) delay (100);
+    Serial.print ("connected\nGetting IP address ... ");
+    while (WiFi.localIP () == IPAddress (0, 0, 0, 0)) delay (100);
+    Serial.println (WiFi.localIP ());
+    
+
+    // create a separate task to ping arduino.com
+    xTaskCreate ([] (void *param)  {
+        ThreadSafePing ping;
+
+        Serial.printf ("Pinging arduino.com %i times ...\n", PING_DEFAULT_COUNT);
+        ping.ping ("arduino.com"); // optional arguments: int count = PING_DEFAULT_COUNT, int interval = PING_DEFAULT_INTERVAL, int size = PING_DEFAULT_SIZE, int timeout = PING_DEFAULT_TIMEOUT
+        if (ping.errText () != NULL) {
+            Serial.printf ("Error %s\n", ping.errText ());
+        } else {
+            Serial.printf ("Ping statistics for %s:\n"
+                        "    Packets: Sent = %i, Received = %i, Lost = %i", ping.target (), ping.sent (), ping.received (), ping.lost ());
+            if (ping.sent ()) {
+                Serial.printf (" (%.2f%% loss)\nRound trip:\n"
+                            "   Min = %.3fms, Max = %.3fms, Avg = %.3fms, Stdev = %.3fms\n", (float) ping.lost () / (float) ping.sent () * 100, ping.min_time (), ping.max_time (), ping.mean_time (), sqrt (ping.var_time () / ping.received ()));
+            } else {
+                Serial.printf ("\n");
+            }
+        }
+
+        vTaskDelete (NULL);
+    }, 
+    "ping_task", 4068, NULL, 1, NULL);
+
+
+    // wait 5s and ping github.com
+    delay (5000);
+
+    ThreadSafePing ping;
+
+    Serial.printf ("Pinging github.com %i times ...\n", PING_DEFAULT_COUNT);
+    ping.ping ("github.com"); // optional arguments: int count = PING_DEFAULT_COUNT, int interval = PING_DEFAULT_INTERVAL, int size = PING_DEFAULT_SIZE, int timeout = PING_DEFAULT_TIMEOUT
+    if (ping.errText () != NULL) {
+        Serial.printf ("Error %s\n", ping.errText ());
+    } else {
+        Serial.printf ("Ping statistics for %s:\n"
+                       "    Packets: Sent = %i, Received = %i, Lost = %i", ping.target (), ping.sent (), ping.received (), ping.lost ());
+        if (ping.sent ()) {
+            Serial.printf (" (%.2f%% loss)\nRound trip:\n"
+                           "   Min = %.3fms, Max = %.3fms, Avg = %.3fms, Stdev = %.3fms\n", (float) ping.lost () / (float) ping.sent () * 100, ping.min_time (), ping.max_time (), ping.mean_time (), sqrt (ping.var_time () / ping.received ()));
+        } else {
+            Serial.printf ("\n");
+        }
+    }
+}
+
+void loop () {
+
+}
